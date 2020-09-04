6ac0bdb31eb45f2ca755af62767acb39e95009ee
tylergreason <tyler.greason@protonmail.com>
2020-09-04 16:38:16 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// let p = require('prompt')
+
+// p.start() 
+
+// const getInput = () => {
+//     p.get(['type stuf'], (err,result) => {
+//     console.log(('you typed'));
+//     console.log(result);
+// })
+// }
+
+// getInput()
+
