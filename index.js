db0eed100deef956cbf7d6e40d37d83950f35a2a
tylergreason <tyler.greason@protonmail.com>
2020-09-04 16:59:09 -0400

    trying to save value from I/O stream to variable outside the stream

--- index.js
+++ index.js
@@ -11,3 +11,28 @@
 
 // getInput()
 
+const readline = require('readline'); 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const recursiveAsyncReadline = () => {
+    rl.question('Type your answer \n', function(answer){
+        if (answer === 'exit'){
+            rl.close()
+        }else{
+            
+            console.log(answer);
+            recursiveAsyncReadline()
+        }
+    })
+}
+
+// recursiveAsyncReadline()
+
+const num = rl.question('enter a number to square', function(answer){
+    return (answer * answer)
+    rl.close()
+})
+console.log(num);
